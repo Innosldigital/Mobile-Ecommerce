f8bf2174f5585227880bb22d104aaf23fb1eb7ae
Innosldigital <platformlead@innosldigital.com>
2025-12-25 11:54:32 +0000

    DB configuration added.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
@@ -20,5 +24,6 @@ if (ENV.NODE_ENV === "production") {
 }
 
 app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
+  console.log(`Server is up and running: ${ENV.PORT}`);
+  connectDB();
 });
