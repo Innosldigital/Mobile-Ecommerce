b2e08c9cc841d1367a0cd4b88837ec252dd4c9b5
Innosldigital <platformlead@innosldigital.com>
2025-12-26 20:56:02 +0000

    Apply stashed changes after resolving asset lock

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./config/env.js";
+import { clerkMiddleware } from "./config/clerk.js";
+import { connectDB } from "./config/db.js";
+import { user } from "./models/user.model.js";
+import { serve } from "./config/inngest.js";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(clerkMiddleware);
+
+app.use("/api/inngest", serve({ client: inngest, functions: functions }));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
